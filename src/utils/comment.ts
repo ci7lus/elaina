0fe7fc317ee640db81717072c0003eb95c6eabb0
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-02-13 22:05:59 +0900

    :children_crossing: 流れるコメントからメンションとコマンドを削除

--- src/utils/comment.ts
+++ src/utils/comment.ts
@@ -2,4 +2,6 @@ export const trimCommentForFlow = (s: string) => {
   return s
     .replace(/https?:\/\/[\w!?/+\-_~;.,*&@#$%()'[\]]+\s?/g, "") // URL削除
     .replace(/#.+\s?/g, "") // ハッシュタグ削除
+    .replace(/\@\w+?\s?/g, "") // メンション削除
+    .replace(/^\/\w+\s?/, "") // コマンド削除
 }
